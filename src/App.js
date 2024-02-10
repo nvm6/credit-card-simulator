d8efbadb1425d072635718d63b5806d890662a7e
nvm6 <nvm4@uic.edu>
2024-02-09 22:39:35 -0600

    redo start page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -9,22 +10,10 @@ function App() {
         <h1> Credit Card Simulator</h1>
         <h2> This simulator is meant to help young adults with credit card management.</h2>
 
-        <div>
-          <label for="username">Username: </label>
-          <input type="text" id="username" name="username" className="logInBox" ></input>
-        </div>
-
-        <br></br>
-
-        <div>
-          <label for="password" >Password: </label>
-          <input type="text" id="password" name="password" className="logInBox"></input>
-        </div>
-
         <br></br>
         <br></br>
 
-        <button type="submit" className="logInButton">Log In</button>
+        <button class="button-class">Start Game</button>
 
 
       </header>
